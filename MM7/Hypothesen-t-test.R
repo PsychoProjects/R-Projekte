a31ebb7d12676b22d9590598dde9c80af6836f3e
Achim <achim@farbstuecke.de>
2024-01-06 18:22:15 +0100

    Einarbeitung der Ergebnisse

--- MM7/Hypothesen-t-test.R
+++ MM7/Hypothesen-t-test.R
@@ -7,37 +7,37 @@ library(effsize)
 library(apa)
 
 # Load the data
-data <- read_excel("Beispiel3.xlsx")
+data <- read_excel("Aufbereitete_Aggregierte_Daten_MM7_JKH.xlsx")
 
 # Inspect the data
 head(data)
 str(data)
 
 # Subset the data for analysis
-analysis_data <- data %>% select(`False Memory`, Surprise)
+analysis_data <- data %>% select(erlebnisbasiert, i1_ue)
 
 # Perform a T-Test
-# Assuming `False Memory` is a binary variable (0 and 1)
-t_test_results <- t.test(Surprise ~ `False Memory`, data = data)
+# Assuming erlebnisbasiert is a binary variable (0 and 1)
+t_test_results <- t.test(i1_ue ~ erlebnisbasiert, data = data)
 
 # Print T-Test results
 print("T-Test Results")
 print(t_test_results)
 
-cd <- cohens_d(Surprise ~ `False Memory`, data = data); # apa
+cd <- cohens_d(i1_ue ~ erlebnisbasiert, data = data); # apa
 print(cd)
-cd <- cohen.d(Surprise ~ `False Memory`, data = data); # effsize
+cd <- cohen.d(i1_ue ~ erlebnisbasiert, data = data); # effsize
 print(cd)
 
 # Non-Parametric Test - Kruskal-Wallis Test
-kruskal_test_results <- kruskal.test(Surprise ~ `False Memory`, data = analysis_data)
+kruskal_test_results <- kruskal.test(i1_ue ~ erlebnisbasiert, data = analysis_data)
 
 # Print Kruskal-Wallis Test results
 print("Kruskal-Wallis Test Results")
 print(kruskal_test_results)
 
 # Optional: Visualizing the data
-ggplot(analysis_data, aes(x = as.factor(`False Memory`), y = Surprise)) +
+ggplot(analysis_data, aes(x = as.factor(erlebnisbasiert), y = i1_ue)) +
   geom_boxplot() +
-  labs(title = "Surprise by `False Memory` Level", x = "`False Memory` Level", y = "Surprise")
+  labs(title = "i1_ue by erlebnisbasiert Level", x = "erlebnisbasiert Level", y = "i1_ue")
 
--- MM7/Hypothesentest.R
+++ MM7/Hypothesentest.R
@@ -3,26 +3,26 @@ library(psych)
 library(readxl)
 
 remove(Data)
-Data <- read_excel("Beispiel.xlsx")
+Data <- read_excel("Aufbereitete_Aggregierte_Daten_MM7_JKH.xlsx")
 
 attach(Data)
 
-describeBy(Discrepancy, `False Memory`)
-describeBy(Surprise, `False Memory`)
+describeBy(i1_dd, `erlebnisbasiert`)
+describeBy(i1_dd, `erlebnisbasiert`)
 
-boxplot(Discrepancy ~ `False Memory`, Data)
-boxplot(Surprise ~ `False Memory`, Data)
+boxplot(i1_dd ~ `erlebnisbasiert`, Data)
+boxplot(i1_dd ~ `erlebnisbasiert`, Data)
 
-wilcox.test(Discrepancy ~ `False Memory`, Data, exact=FALSE, conf.int=TRUE)
-wilcox.test(Surprise ~ `False Memory`, Data, exact=FALSE, conf.int=TRUE)
+wilcox.test(i1_dd ~ `erlebnisbasiert`, Data, exact=FALSE, conf.int=TRUE)
+wilcox.test(i1_dd ~ `erlebnisbasiert`, Data, exact=FALSE, conf.int=TRUE)
 
 par(mfrow=c(1,2))
 intervals <- seq(-0.5, 3.5)
-hist(Surprise[`False Memory` == 0], breaks=intervals)
-hist(Surprise[`False Memory` == 1], breaks=intervals)
+hist(i1_dd[`erlebnisbasiert` == 0], breaks=intervals)
+hist(i1_dd[`erlebnisbasiert` == 1], breaks=intervals)
 
 intervals <- seq(-0.5, 1.5)
-hist(Discrepancy[`False Memory` == 0], breaks=intervals)
-hist(Discrepancy[`False Memory` == 1], breaks=intervals)
+hist(i1_dd[`erlebnisbasiert` == 0], breaks=intervals)
+hist(i1_dd[`erlebnisbasiert` == 1], breaks=intervals)
 
 par(mfrow=c(1,1))
