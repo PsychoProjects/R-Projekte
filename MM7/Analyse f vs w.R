a24f9ae3831881b64d33a74a447bcf5dced68258
Achim <achim@farbstuecke.de>
2024-01-16 11:49:31 +0100

    Formatierungen

--- MM7/Analyse f vs w.R
+++ MM7/Analyse f vs w.R
@@ -5,8 +5,8 @@ library(effsize)
 # Einlesen der Daten
 data <- read_excel("Mittelwerte_MM7_korrigiert.xlsx")
 
-f_data <- data$i1_dd_f_MW + (data$i1_ue_f_MW -1 ) / 2
-w_data <- data$i1_dd_w_MW + (data$i1_ue_w_MW -1 ) / 2
+f_data <- data$i1_dd_f_MW + (data$i1_ue_f_MW - 1) / 2
+w_data <- data$i1_dd_w_MW + (data$i1_ue_w_MW - 1) / 2
 differences <- f_data - w_data
 
 n_data <- data.frame(w_data, f_data, differences)
@@ -23,4 +23,7 @@ print(c)
 boxplot(differences)
 hist(differences)
 
-
+par(mfrow = c(1,2))
+hist(w_data)
+hist(f_data)
+par(mfrow = c(1,1))
