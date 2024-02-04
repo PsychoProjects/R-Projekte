a0c4bb91a1f44746612265320194d5589534f8be
Achim <achim@farbstuecke.de>
2024-02-04 14:43:21 +0100

    Kommentare überarbeitet
    This reverts commit 8fcaea55ee5940f84a77cbf4a74f86dde02294a1.

--- MM7/mm7_analysis_lib.R
+++ MM7/mm7_analysis_lib.R
@@ -14,22 +14,23 @@ daten <- read_excel("Mittelwerte_MM7_korrigiert.xlsx")
 
 # Funktion für die deskriptive Analyse
 fn_descriptive_analysis <- function(i1_w_MW, i1_f_MW){
-  cat(">>> i1_w_MW\n")
+  cat("summary(i1_w_MW, digits = 3)\n")
   summary(i1_w_MW, digits = 3) %>% print()
-  cat("\n")
+  cat("describe(i1_w_MW, skew = FALSE)\n")
   describe(i1_w_MW, skew = FALSE)  %>% print()
   cat("\n\n")
   
-  cat(">>> i1_f_MW\n")
+  cat("summary(i1_f_MW, digits = 3)\n")
   summary(i1_f_MW, digits = 3) %>% print()
-  cat("\n")
+  cat("describe(i1_f_MW, skew = FALSE)\n")
   describe(i1_f_MW, skew = FALSE) %>% print()
   cat("\n\n")
   
+  cat("difference = i1_f_MW - i1_w_MW\n")
   differences = i1_f_MW - i1_w_MW
-  cat(">>> i1_f_MW - i1_w_MW\n")
+  cat("summary(differences, digits = 3)\n")
   summary(differences, digits = 3) %>% print()
-  cat("\n")
+  cat("describe(differences, skew = FALSE)\n")
   describe(differences, skew = FALSE) %>% print()
 }
 
@@ -52,6 +53,7 @@ fn_graphical_analysis <- function(i1_w_MW, i1_f_MW, title = "") {
 fn_tTest <- function (i1_w_MW, i1_f_MW) {
   cat(">>> Vorbedingungen prüfen\n")
 
+  cat("difference = i1_f_MW - i1_w_MW\n")
   differences = i1_f_MW - i1_w_MW
   
   cat(">>> Grafische Beurteilung der Normalverteilung der Differenzen\n")
@@ -59,17 +61,17 @@ fn_tTest <- function (i1_w_MW, i1_f_MW) {
        main = "Histogramm der Differenzen", xlab = "Differenzen", 
        ylab = "Häufigkeit", col = "lightblue", border = "black")
   
-  cat(">>> Shapiro-Wilk-Test zur Überprüfung der Normalverteilung")
+  cat("stats::shapiro.test(differences)\n")
   stats::shapiro.test(differences) %>% print() 
 
-  cat(">>> t.test(i1_f_MW, i1_w_MW, paired = TRUE, alternative = two.sided\n")
+  cat("stats::t.test(i1_f_MW, i1_w_MW, paired = TRUE, alternative = two.sided)\n")
   result <- stats::t.test(i1_f_MW, i1_w_MW, paired = TRUE, 
                           alternative = "two.sided") %>% print() 
   
   z <- fn_zValue_from_pValue(result$p.value)
   cat("z-value: ", z, "\n\n")
   
-  cat(">>> Effektstärke\n")
+  cat("effsize::cohen.d(i1_f_MW, i1_w_MW, paired = TRUE, alternative = two.sided)\n")
   effsize::cohen.d(i1_f_MW, i1_w_MW, paired = TRUE, alternative = "two.sided") %>% print() 
 }
 
@@ -89,11 +91,12 @@ fn_pearson_r <- function (N, zValue) {
 fn_wilcoxon <- function (i1_w_MW, i1_f_MW) {
   cat(">>> Vorbedingungen prüfen\n")
 
+  cat("difference = i1_f_MW - i1_w_MW\n")
   differences = i1_f_MW - i1_w_MW
   
   boxplot(differences, main = "Boxplot der Differenzen", ylab = "Differenzen", col = "lightblue")
   
-  cat(">>> wilcox.test(i1_f_MW, i1_w_MW, paired = TRUE, alternative = two.sided)")
+  cat("wilcox.test(i1_f_MW, i1_w_MW, paired = TRUE, alternative = two.sided)")
   result <- stats::wilcox.test(i1_f_MW, i1_w_MW, paired = TRUE, 
                                alternative = "two.sided", conf.int = TRUE) %>% print() 
   
@@ -107,9 +110,10 @@ fn_wilcoxon <- function (i1_w_MW, i1_f_MW) {
 
 # Funktion für die Prüfung und Durchführung des Vorzeichen-Tests
 fn_sign <- function (i1_w_MW, i1_f_MW) {
+  cat("difference = i1_f_MW - i1_w_MW\n")
   differences = i1_f_MW - i1_w_MW
 
-  cat(">>> SIGN.test(i1_f_MW, i1_w_MW, md=0, paired = TRUE, alternative = two.sided")
+  cat("SIGN.test(i1_f_MW, i1_w_MW, md=0, paired = TRUE, alternative = two.sided")
   result <- BSDA::SIGN.test(i1_f_MW, i1_w_MW, md=0, paired = TRUE, 
                             alternative = "two.sided", conf.int = TRUE) %>% print() 
   
@@ -124,28 +128,36 @@ fn_sign <- function (i1_w_MW, i1_f_MW) {
 # Funktion für die Analyse der Summen aus Diskrepanz und Überraschung
 fn_sum_analysis <- function() {
   # Bildung der Summen bei Normierung der ue-Daten auf Werte in [0;1]
-  f_daten <- daten$i1_dd_f_MW + (daten$i1_ue_f_MW - 1) / 2
-  w_daten <- daten$i1_dd_w_MW + (daten$i1_ue_w_MW - 1) / 2
+  cat("i1_f_MW <- daten$i1_dd_f_MW + (daten$i1_ue_f_MW - 1) / 2\n")
+  i1_f_MW <- daten$i1_dd_f_MW + (daten$i1_ue_f_MW - 1) / 2
+  cat("i1_w_MW <- daten$i1_dd_w_MW + (daten$i1_ue_w_MW - 1) / 2\n")
+  i1_w_MW <- daten$i1_dd_w_MW + (daten$i1_ue_w_MW - 1) / 2
 
-  fn_descriptive_analysis(i1_w_MW = w_daten, i1_f_MW = f_daten)
+  cat("\n")
+  fn_descriptive_analysis(i1_w_MW = i1_w_MW, i1_f_MW = i1_f_MW)
 
-  fn_graphical_analysis(i1_w_MW = w_daten, i1_f_MW = f_daten, title = "Summen der Werte")
+  fn_graphical_analysis(i1_w_MW = i1_w_MW, i1_f_MW = i1_f_MW, title = "Summen der Werte")
   
   cat("\n\n")
-  fn_tTest(i1_w_MW = w_daten, i1_f_MW = f_daten)  
+  fn_tTest(i1_w_MW = i1_w_MW, i1_f_MW = i1_f_MW)  
 }
 
 # Funktion zur Analyse von Korrelationen
 fn_correlation_analysis <- function() {
+  cat("i1_ue <- c(daten$i1_ue_w_MW, daten$i1_ue_f_MW)\n")
   i1_ue <- c(daten$i1_ue_w_MW, daten$i1_ue_f_MW)
+  cat("i1_dd <- c(daten$i1_dd_w_MW, daten$i1_dd_f_MW)\n")
   i1_dd <- c(daten$i1_dd_w_MW, daten$i1_dd_f_MW)
   
   scatter.smooth(i1_dd, i1_ue, main = "Diskrepanzerkennungswert vs. Initialer Überraschungswert", 
                  xlab = "Diskrepanzerkennungswert", ylab = "Initialer Überraschungswert", 
                  pch = 19, col = "lightblue")
   
+  cat("cor.test(i1_dd, i1_ue, method = pearson)\n")
   cor.test(i1_dd, i1_ue, method = "pearson") %>% print()
+  cat("cor.test(i1_dd, i1_ue, method = kendall)\n")
   cor.test(i1_dd, i1_ue, method = "kendall") %>% print()
+  cat("cor.test(i1_dd, i1_ue, method = spearman)\n")
   cor.test(i1_dd, i1_ue, method = "spearman", exact = FALSE) %>% print()
 }
 
